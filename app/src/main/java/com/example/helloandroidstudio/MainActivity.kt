53ab1b6ed0a89476663cf69b092b9647fdaa0177
ViniciusPolo <admviniciuspolo@gmail.com>
2024-04-21 14:56:54 -0300

    page 90 - inicio entendimento classe R e uso Logcat

--- app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.helloandroidstudio
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +17,11 @@ import com.example.helloandroidstudio.ui.theme.HelloAndroidStudioTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.v(TAG,"log de Verbose")
+        Log.d(TAG,"log de Debug")
+        Log.i(TAG,"log de Info")
+        Log.w(TAG,"log de Alerta")
+        Log.e(TAG, throw RuntimeException("teste de erro"))
         setContent {
             HelloAndroidStudioTheme {
                 // A surface container using the 'background' color from the theme
@@ -32,15 +39,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $name!" + R.string.app_name ,
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun GreetingPreview() {
     HelloAndroidStudioTheme {
-        Greeting("Android")
+        Greeting("Semi Deus")
     }
 }
