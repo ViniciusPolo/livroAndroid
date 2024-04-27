4b764e7afb6fcaad427cffc6e949b49ef09c26b4
ViniciusPolo <admviniciuspolo@gmail.com>
2024-04-27 13:21:03 -0300

    primeira tela Login

--- app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
@@ -1,35 +1,55 @@
 package com.example.helloandroidstudio
 
-import android.content.ContentValues.TAG
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.helloandroidstudio.ui.theme.HelloAndroidStudioTheme
+import org.w3c.dom.Text
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            HelloAndroidStudioTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Vinicius")
-                }
+        setContentView(R.layout.activity_main);
+        val btLogin = findViewById<Button>(R.id.btLogin)
+        btLogin.setOnClickListener {
+            val tLogin = findViewById<TextView>(R.id.tLogin)
+            val tSenha = findViewById<TextView>(R.id.tSenha)
+            val login = tLogin.text.toString()
+            val senha = tSenha.text.toString()
+            if ("vinicius".equals(login) && "123".equals(senha)) {
+                showAlert(this, "Login realizado", "Bem Vindo " + tLogin.text)
+            } else {
+                showAlert(this, "Não foi possível entrar", "Tente novamente")
+
             }
         }
+
     }
 }
+
+fun showAlert(context: Context, title: String, message: String) {
+    val builder = AlertDialog.Builder(context)
+    builder.setTitle(title)
+    builder.setMessage(message)
+
+    // Adiciona um botão "OK" ao alerta
+    builder.setPositiveButton("OK") { dialog, which ->
+        // Aqui você pode adicionar alguma ação a ser executada quando o botão "OK" for clicado
+    }
+
+    // Cria e exibe o alerta
+    val dialog = builder.create()
+    dialog.show()
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
