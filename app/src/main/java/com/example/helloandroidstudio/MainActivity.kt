88216534bd15c76c87feefcadb3d69fa14125dca
ViniciusPolo <admviniciuspolo@gmail.com>
2024-06-16 12:33:58 -0300

    understanding Activities, page 106 save instance state

--- app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.ContentValues.TAG
 import android.content.Context
 import android.nfc.Tag
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
@@ -41,10 +42,16 @@ class MainActivity : ComponentActivity() {
         Log.i(TAG,"log de Stop")
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        // When we quit app, and dead Activity
-        Log.i(TAG,"log de Destroy")
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        // When we quit app, and dead Activity
+//        Log.i(TAG,"log de Destroy")
+//    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // Pay attention, can get conflict with destroy, use when we rotate cellphone
+        Log.i(TAG, "Log de Save Instance State")
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
